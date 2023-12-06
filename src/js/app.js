1e6873218118d83656a5287ff9d033e40478e052
tetianazaporozhets <web.smm.zty@gmail.com>
2023-12-06 18:54:56 +0100

    6.12

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,40 @@
-$( document ).ready(function() {
-    //nav btn
-    $('.nav__btn').click(function(){
-        $('.nav').toggleClass('active');
-    })
+$(document).ready(function () {
+  $(".gallery__slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: false,
+    arrows: false,
+    speed: 300,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  //nav btn
+  $(".nav__btn").click(function () {
+    $(".nav").toggleClass("active");
+  });
 
-  /*   $(document).ready(function(){
-        $('.gallery__slider').slick({
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          arrows: false,
-          dots: true,
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 3000,
-        });
-      }); */
-      $(document).ready(function(){
-      $('.gallery__slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        dots: true,
-        arrows: false,
-        speed: 300,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-          {
-            breakpoint: 767,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            }
-          }
-        ]
-      });
-    });
+  $(".custom-arrows .prev-arrow").click(function () {
+    $(".gallery__slider").slick("slickPrev");
+  });
+
+  $(".custom-arrows .next-arrow").click(function () {
+    $(".gallery__slider").slick("slickNext");
+  });
 });
